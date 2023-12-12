0d3c1935820249ede8d61f5f5d09a5ea91363fd2
Dima <dimakarpovich1@gmail.com>
2023-12-12 12:20:15 +0300

    edited files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
     return (
         <nav className="green lighten-1">
             <div className="nav-wrapper">
-                <a href="!#" className="brand-logo">
+                <a href="/" className="brand-logo">
                     React Shop
                 </a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
